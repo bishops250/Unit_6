ffb2bc334449af973f20c456dd869245bf493756
bishops250 <bishops250@gmail.com>
2019-12-06 12:06:08 -0500

    Work in Progress...

---
+++ src/TraverseArrays.java
@@ -0,0 +1,35 @@
+public class TraverseArrays {
+    public static int countEven(int[] nums) {
+
+        int num_evens=0;
+
+        for(int i=0; i<= nums.length; ++i) {
+            if(nums[i]%2==0) {
+                num_evens += 1;
+            }
+            else {
+
+            }
+        }
+
+    }
+
+    public static int[] buildArray(int desiredLength) {
+
+    }
+    public static boolean sum28(int[] numbers) {
+
+    }
+
+    public static int[] zeroMax(int[] original) {
+
+    }
+
+
+
+
+
+    public static void main(String[]args) {
+
+    }
+}
