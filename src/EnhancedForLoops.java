30cfbf1f80a6cfd5d00b07ffdb51f3deb94af676
bishops250 <biishops250@gmail.com>
2019-12-12 19:03:39 -0500

    Finished.

--- src/EnhancedForLoops.java
+++ src/EnhancedForLoops.java
@@ -2,7 +2,7 @@ public class EnhancedForLoops {
 
     public static String printStandardFor(String[] arr){
         String printed_string= "";
-        for(int i=0; i<= arr.length;++i) {
+        for(int i=0; i<= arr.length-1;++i) {
             printed_string += arr[i] + " ";
         }
         return printed_string;
@@ -11,7 +11,7 @@ public class EnhancedForLoops {
     public static String printStandardFor(int[] arr) {
 
         String print_value = "";
-        for (int i = 0; i <= arr.length; ++i ) {
+        for (int i = 0; i <= arr.length-1; ++i ) {
             print_value += arr[i] + " ";
         }
         return print_value;
@@ -19,7 +19,7 @@ public class EnhancedForLoops {
 
     public static String printStandardFor(double[] arr) {
         String printed_value="";
-        for(int i=0; i<= arr.length; ++i) {
+        for(int i=0; i<= arr.length-1; ++i) {
             printed_value += arr[i] + " ";
         }
         return printed_value;
@@ -54,7 +54,7 @@ public class EnhancedForLoops {
         String print_string= "";
         int i=0;
         while(i<arr.length) {
-            print_string += arr[i] + "";
+            print_string += arr[i] + " ";
             ++i;
         }
         return print_string;
@@ -83,9 +83,10 @@ public class EnhancedForLoops {
 
     public static void main(String[]args) {
 
-        int integer_array[]={1,2,3,4,5};
-        double double_array[]={1.1,2.2,3.3,4.4,5.5};
-        String string_array[]= {"Bob","Jeff","Larry","Aron","Blake"};
+        int[] integer_array ={1,2,3,4,5};
+        double[] double_array ={1.1,2.2,3.3,4.4,5.5};
+        String[] string_array = {"Bob","Jeff","Larry","Aron","Blake"};
+        
 
     }
 }
