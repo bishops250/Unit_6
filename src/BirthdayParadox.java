f16131b3bea3684d7739cd13e7256c56b81c64dd
bishops250 <bishops250@gmail.com>
2020-01-08 11:02:06 -0500

    birthday

--- src/BirthdayParadox.java
+++ src/BirthdayParadox.java
@@ -1,2 +1,69 @@
+/*
+Creator: Aron Bishop
+AP Computer Science
+Date: January 8, 2019 10:00am
+Program name: BirthdayParadox
+
+BirthdayParadox tests the paradox that in a room of 23 approximately 50% of the individuals in a room, will share a
+birth date.
+
+
+
+
+
+ */
+
+import java.util.Scanner;
+
 public class BirthdayParadox {
+
+
+    public static int[] dates(int individuals) {
+        int[] birth_date = new int[individuals];
+
+
+        for (int i = 0; i < individuals; i++) {
+            int random_num = (int) (Math.random() * 364 + 1);
+            birth_date[i] = random_num;
+
+        }
+
+        return birth_date;
+    }
+
+    public static boolean hasDupes(int[] list) {
+
+
+        for (int i = 0; i <= list.length - 1; ++i) { //First Loop Chooses Number checking for Duplicates
+            for (int count = i + 1; count <= list.length - 1; ++count) { //Second Loop checks the numbers to the side of it.
+                if (list[i] == list[count]) {
+                    return true;
+                }
+
+            }
+
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        int individuals;
+
+        System.out.println();
+        System.out.println("Please select the amount of individuals in the room: ");
+
+
+        double num = 0.0;
+        for (int count = 0; count < 1000; count++) {
+            int[] data = dates(23);
+
+            if (hasDupes(data)) {
+                num++;
+            }
+
+        }
+        double percentage= num/1000 * 100;
+        System.out.println(percentage);
+    }
 }
